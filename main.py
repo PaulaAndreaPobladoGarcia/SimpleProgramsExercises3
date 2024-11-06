d3f02d2167927ba4fbf490e15e4414af7d0b4fae
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-11-06 12:07:31 -0500

    solucion_tabla_de_multiplicar

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-num1 = int(input("ingresa el primer numero: "))
-num2 = int(input("ingresa el segundo numero: "))
-
-inicio = min(num1, num2)+ 1
-fin = max(num1, num2)
-
-suma = 0
-for i in range(inicio, fin): 
-    suma += i
-    print("la suma es", suma)
+for i in range(1, 11):  
+    
+    for j in range(1, 11):  
+        print(f"{i * j:3}", end=" ")  
+    print()  
