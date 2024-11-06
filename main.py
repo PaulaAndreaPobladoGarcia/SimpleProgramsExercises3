eb9b70360f99c2abeebe83ebf5055bd7d455c913
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-11-06 12:56:54 -0500

    solucion_e

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-n = 1  
-fraccion = 0.5  
-suma = 0  
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
 
-print(f"{'Potencia':<10}{'Fracción':<15}{'Suma'}")
+def estimar_e():
+    n = 10  
+    suma = 0
+    anterior = 0
+    while True:
+        
+        actual = factorial(n)
+        
+        
+        suma += actual
+        
+        if abs(actual - anterior) < 0.0001:
+            break
+        
+        anterior = actual
+        
+        n += 1
+    
+    return suma
 
-while fraccion > 0.000001:
-    suma += fraccion  
-    print(f"{n:<10}{fraccion:<15}{suma}") 
-    n += 1  
-    fraccion /= 2  # La siguiente
-
+e_aproximado = estimar_e()
+print(f"Valor aproximado de e: {e_aproximado}")
