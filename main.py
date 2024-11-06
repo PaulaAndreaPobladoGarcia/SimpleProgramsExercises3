dd4710e24fa3203beae676a32331635413263542
PaulaAndreaPobladoGarcia <182712699+PaulaAndreaPobladoGarcia@users.noreply.github.com>
2024-11-06 11:57:06 -0500

    solucion_Suma _entre _números

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = int(input("ingrese el nuemro deseado: "))
+num1 = int(input("ingresa el primer numero: "))
+num2 = int(input("ingresa el segundo numero: "))
 
-pontecia = 1
+inicio = min(num1, num2)+ 1
+fin = max(num1, num2)
 
-for i in range (n+1):
-   print(pontecia, end="")
-   pontecia = pontecia * 2
- 
+suma = 0
+for i in range(inicio, fin): 
+    suma += i
+    print("la suma es", suma)
